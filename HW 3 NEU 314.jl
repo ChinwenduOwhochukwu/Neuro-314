856031e36ce637d13f5cce1a87d542a0f5809bf2
NEU_314 CO <chinwenduowhochukwu41@gmail.com>
2019-10-04 00:13:34 -0400

    this is finished until part e because Julia keep crashing

--- HW 3 NEU 314.jl
+++ HW 3 NEU 314.jl
@@ -2,7 +2,7 @@ using JLD
 using PyPlot
 EC= imread("/Users/chinwenduowhochukwu/NEU_314/el_capitan.png")
 #1A-A1: to visualize el capitan
-image= imshow(convert(Array{Float32,3},EC))
+data= imshow(convert(Array{Float32,3},EC))
 clf(); #clear it
 
 #1A-A2: to quantify the matrix
@@ -16,8 +16,16 @@ function show_channels(image)
     G= image[:,:,2];
     return R, G, B
 end
+#1A-C flaggin for bug
+function show_channels(image)
+    B= image[:,:,3];
+    R= image[:,:,1];
+    G= image[:,:,2];
+    println("BUG BUG BUG")
+    return R, G, B
+end
 
-#1A-C flagging for Bug
+#1A-D flagging for reset
 function show_channels(image)
     B= image[:,:,3];
     R= image[:,:,1];
@@ -25,3 +33,13 @@ function show_channels(image)
     println("Reset with git resert--mixed")
     return R, G, B
 end
+
+#1A-E making a new image2
+#cat(G,B,R, dims=3 )
+image2= cat(show_channels(EC)[2], show_channels(EC)[3], show_channels(EC)[1], dims= 3)
+
+#to display image2 and EC(the original image) individually
+imshow(image2)
+imshow(EC)
+#to display image2 and EC together on one plot
+imshow(image2), subplot(1,2,2); imshow(EC), subplot(1,2,1), axis("off"), title("EC and Image2"), figure(figsize=[640,360])
