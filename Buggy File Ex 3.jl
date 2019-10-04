8303af0f6eb2c36ef93995868f2c9d9e51122ebf
NEU_314 CO <chinwenduowhochukwu41@gmail.com>
2019-10-03 21:53:53 -0400

    Buggy file for exercise 3

---
+++ Buggy File Ex 3.jl
@@ -0,0 +1,21 @@
+#Buggy file Ex 3
+using JLD
+using PyPlot
+EC= imread("/Users/chinwenduowhochukwu/NEU_314/el_capitan.png")
+#1A-A1: to visualize el capitan
+image= imshow(convert(Array{Float32,3},EC))
+clf(); #clear it
+
+#1A-A2: to quantify the matrix
+size(EC)
+
+#1A-C flagging for Bug
+function show_channels(image)
+    B= image[:,:,3];
+    R= image[:,:,1];
+    G= image[:,:,2];
+    println("BUG, BUG, BUG")
+    return R, G, B
+end
+
+#this is the end
